840d786157d3ff595e556df4b4a691e719139d79
jasondonohue20 <jasondonohue20@augustana.edu>
2023-03-09 20:40:45 -0600

    Update march_madness.R

--- march_madness.R
+++ march_madness.R
@@ -3,3 +3,9 @@ library(ggplot2)
 
 game_data <- read.csv("2023_game_data.csv")
 tournament_team_data <- read.csv("tournament_team_data.csv")
+
+game_data <- game_data %>%
+  select(TEAM, KENPOM.ADJUSTED.EFFICIENCY,BARTTORVIK.ADJUSTED.EFFICIENCY)
+
+tournament_team_data <- tournament_team_data %>%
+  select(TEAM, KENPOM.ADJUSTED.EFFICIENCY,BARTTORVIK.ADJUSTED.EFFICIENCY)
