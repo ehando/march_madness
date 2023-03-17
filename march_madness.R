31e4ab83f69944a2c0fc6db0f7d5a8a31e0c01ff
ehando <113647236+ehando@users.noreply.github.com>
2023-03-16 19:39:51 -0500

    update
    right version

--- march_madness.R
+++ march_madness.R
@@ -2,44 +2,13 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 
-game_data <- read.csv("2023_game_data.csv")
-tournament_team_data <- read.csv("tournament_team_data.csv")
-new_tournament_data <- read.csv("2023 Tournament Data.csv")
+game_data <- read.csv("2023_game_data.csv") %>%
+  select(TEAM, KENPOM.ADJUSTED.EFFICIENCY,
+         BARTTORVIK.ADJUSTED.EFFICIENCY,
+         TURNOVER..,POINTS.PER.POSSESSION.DEFENSE) %>%
+  distinct(TEAM, .keep_all = TRUE)
 
-efficiency_game_data <- game_data %>%
-  select(TEAM, KENPOM.ADJUSTED.EFFICIENCY,BARTTORVIK.ADJUSTED.EFFICIENCY)
+game_data$TOTAL.SCORE <- game_data$KENPOM.ADJUSTED.EFFICIENCY *
+game_data$BARTTORVIK.ADJUSTED.EFFICIENCY *
+game_data$TURNOVER.. / game_data$POINTS.PER.POSSESSION.DEFENSE
 
-efficiency_tournament_team_data <- tournament_team_data %>%
-  select(TEAM, KENPOM.ADJUSTED.EFFICIENCY,BARTTORVIK.ADJUSTED.EFFICIENCY)
-
-
-merged_average <- full_join(efficiency_game_data, efficiency_tournament_team_data) %>%
- group_by(TEAM) %>%
- summarise(KENPOM.ADJUSTED.EFFICIENCY = mean(KENPOM.ADJUSTED.EFFICIENCY, na.rm = TRUE), BARTTORVIK.ADJUSTED.EFFICIENCY = mean(BARTTORVIK.ADJUSTED.EFFICIENCY, na.rm = TRUE)) %>%
-  arrange(desc(TEAM))
-
-
-merged_turnover <- full_join(game_data, tournament_team_data) %>%
-  group_by(TEAM)%>%
-  summarise(TURNOVER.. = mean(TURNOVER.., na.rm = TRUE)) %>%
-  arrange(order(TURNOVER..))
-
-merged_defense_per <- full_join(game_data, tournament_team_data) %>%
-  group_by(TEAM) %>%
-  summarise(POINTS.PER.POSSESSION.DEFENSE = mean(POINTS.PER.POSSESSION.DEFENSE, na.rm = TRUE)) %>%
-  arrange(order(POINTS.PER.POSSESSION.DEFENSE))
-
-
-new_tournament_data_select <- new_tournament_data %>%
-  select(TEAM, KENPOM.ADJUSTED.EFFICIENCY, BARTTORVIK.ADJUSTED.EFFICIENCY, FREE.THROW.., TURNOVER..)
-
-
-  
-
-
-
-
-      
-
-  
-   
